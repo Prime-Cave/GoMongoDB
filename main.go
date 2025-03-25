49a14c8149db109b277fcd2dcc942b4e0bbbf3b4
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-03-25 08:53:28 +0200

    Untested Changes

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/prime-cave/mongo-golang/controllers"
+	"gopkg.in/mgo.v2"
+)
+
 func main(){
-	
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe(":8080", r)
+}
+
+func getSession() *mgo.Session{
+	 s, err := mgo.Dial("mongodb://localhost:27107")
+	 if err != nil {
+		panic(err)
+	 }
+	 return s
 }
